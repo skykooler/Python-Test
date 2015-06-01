94bf78a2c0022ea0a10b8c22a17663906a62c1b3
Matt Havard <matt.havard@idea-evolver.com>
2015-06-01 15:36:53 -0400

    More clearly written instructions

--- problem.py
+++ problem.py
@@ -58,18 +58,18 @@
 # outputs are.  You may review and modify run-tests.py as much as you want, but
 # it will not score or lose you any points
 
-# Included in the top level directory are four "hard" test cases that are grids
-# of 10, 25, 50, 100, and 1000 cells.  These were randomly generated using
-# generate-hard-test-case.py, and they do not come with an expected output.
-# You may generate test cases of various dimensions using
-# generate-test-case.py, but the ability of your algorithm to solve extra test
-# cases you've created will not be considered in our evaluation of this test.
-# Your algorithm should be able to find a correct solution in a timely manner
-# up to the 100x100 grid.  The 1000x1000 grid is "Extra Credit", as it were. If
-# you work out an algorithm that can solve the 1000x1000 grid, but it takes
-# more than 10 seconds to do so, please note that in a comment and let us know
-# when you e-mail us your finished product.  All other test cases should take
-# your algorithm no more than 3 seconds.
+# Included in the top level directory are four "hard" test cases that are
+# square grids of side lengths 10, 25, 50, 100, and 1000 cells.  These were
+# randomly generated using generate-hard-test-case.py, and they do not come
+# with an expected output.  You may generate test cases of various dimensions
+# using generate-test-case.py, but the ability of your algorithm to solve extra
+# test cases you've created will not be considered in our evaluation of this
+# test.  Your algorithm should be able to find a correct solution in a timely
+# manner up to the 100x100 grid.  The 1000x1000 grid is "Extra Credit", as it
+# were. If you work out an algorithm that can solve the 1000x1000 grid, but it
+# takes more than 10 seconds to do so, please note that in a comment and let us
+# know when you e-mail us your finished product.  All other test cases should
+# take your algorithm no more than 3 seconds.
 
 # Finally, you may not use third party libraries to complete this test.  You
 # may only use the libraries available on a fresh Python 2.7 install.  I doubt
