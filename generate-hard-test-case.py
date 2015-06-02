9f0dc8a93b0832dac6809abcedb884bfbf3eaf56
Matt Havard <matt.havard@idea-evolver.com>
2015-06-01 22:40:45 -0400

    clean up generate-hard-test.py

--- generate-hard-test-case.py
+++ generate-hard-test-case.py
@@ -1,9 +1,8 @@
 
-from random import randint, random, choice
+from random import random, choice
 import sys
 m, n = map(int, sys.argv[1:])
 with open('hard-test-case{0}x{1}.txt'.format(m, n), 'w') as test_file:
-    test_file.write('%d\n' % m)
-    test_file.write('%d\n' % n)
-    matrix = '\n'.join([' '.join([choice(['0', '1']) for i in range(n)]) for i in range(m)])
+    test_file.write('%d\n%d\n' % (m, n))
+    matrix = '\n'.join([' '.join([choice('01') for i in range(n)]) for i in range(m)])
     test_file.write(matrix)
