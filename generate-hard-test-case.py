013991d9ac149e960cc9e3afaedd793708d81eea
Matt Havard <matt.havard@idea-evolver.com>
2015-06-01 22:42:41 -0400

    Consistent string formatting

--- generate-hard-test-case.py
+++ generate-hard-test-case.py
@@ -3,6 +3,6 @@ from random import random, choice
 import sys
 m, n = map(int, sys.argv[1:])
 with open('hard-test-case{0}x{1}.txt'.format(m, n), 'w') as test_file:
-    test_file.write('%d\n%d\n' % (m, n))
+    test_file.write('{0}\n{1}\n'.format(m, n))
     matrix = '\n'.join([' '.join([choice('01') for i in range(n)]) for i in range(m)])
     test_file.write(matrix)
