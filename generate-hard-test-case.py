c6fca5193f55f71d358973099ee8119be024ed0e
Matt Havard <matt.havard@idea-evolver.com>
2015-06-01 23:49:24 -0400

    Better documentation for generate-hard-test-case.py

--- generate-hard-test-case.py
+++ generate-hard-test-case.py
@@ -1,3 +1,9 @@
+"""Usage:
+
+At the command line, if you do `python generate-hard-test-case.py 15 78`,
+this script will produce a file called `hard-test-case15x78.txt` that
+contains a matrix with 15 rows and 78 columns.
+"""
 
 from random import random, choice
 import sys
